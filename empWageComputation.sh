893bd6e0d84cee1fac623fa53c6df4a78b32cf55
alekhya87 <davulurialekhya87909.com>
2020-10-05 20:59:30 +0530

    Add Part Time Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash/ -x
+
+#start
+
 echo " Welcome to Employee Wage Computation Program on Master Branch "
+
+#Uc1
+
 isPresent=1
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
@@ -8,6 +14,9 @@ then
 else
 	echo "Employee is Absent"
 fi
+
+#Uc2
+
 isPresent=1
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
@@ -18,3 +27,21 @@ then
 else
    salary=0
 fi
+
+#Uc3
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
+
+if [ $isFullTime -eq $randomCheck ]
+then
+	empHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+	empHrs=4
+else
+	empHrs=0
+fi
+salary=$(($empHrs*$empRatePerHr))
