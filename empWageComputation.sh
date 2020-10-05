56caeea13d274b0abdf41b0761cb709a9c8a28a1
alekhya87 <davulurialekhya87909.com>
2020-10-06 01:20:38 +0530

    Employee Wage Data stored

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -197,3 +197,48 @@ do
 done
    totalSalary=$(($totalWorkHours*$empRatePerHr))
 	echo ${dailyWage[@]}
+
+#Uc9
+
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=4
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+declare -A dailyWage
+
+function getWorkingHours(){
+   local $empCheck=$1
+   case $empCheck in
+      $isFullTime)
+         workHours=8
+         ;;
+      $isPartTime)
+         workHours=4
+         ;;
+      *)
+         workHours=0
+         ;;
+      esac
+      echo $workHours
+}
+function getEmWage(){
+   local empHr=$1
+   wage=$(($empHr*$empRatePerHr))
+   echo $wage
+}
+while [[ $totalWorkHours -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+   ((totalWorkingDays++))
+   workHours="$( getWorkingHours $((RANDOM%3)) )"
+   totalWorkHours=$(($totalWorkHours+$workHours))
+   day="Day"
+	dailyWage["Day "$totalWorkingDays]="$( getEmpWage $empHrs )"
+done
+   totalSalary=$(($totalWorkHours*$empRatePerHr))
+   echo ${dailyWage[@]}
+	echo ${!dailyWage[@]}
