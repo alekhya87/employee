b57575a6dd854ae67897fcd91a4eb655cb0321d7
alekhya87 <davulurialekhya87909.com>
2020-10-05 22:29:14 +0530

    Employee Wage Till Reaches done

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -92,3 +92,34 @@ do
 	salary=$(($empHrs*$empRatePerHr))
 	totalSalary=$(($totalSalary+$salary))
 done
+
+#Uc6
+
+isPartTime=1
+isFullTime=2
+totaleSalary=0
+maxHrsInMonth=100
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+   ((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+      case $empCheck in
+      $isFullTime)
+         empHrs=8
+         ;;
+      $isPartTime)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+      esac
+
+   totalSalary=$(($totalSalary+$salary))
+done
+	totalSalary=$(($totalEmpHr*$empRatePerHr))
